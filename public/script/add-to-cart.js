f7e2d972437c892ba9c1becd326510d7ac1e744b
Sam <samuelcourchesne2004@gmail.com>
2025-04-23 12:07:35 -0400

    feat: add the display of the number of item in basket

--- public/script/add-to-cart.js
+++ public/script/add-to-cart.js
@@ -1,17 +1,23 @@
 
-
+let nbItems = 0;
+const nbItemsElement = document.querySelector(".nb-items-in-cart");
 
 
 export function initAddToCart() {
 
+
     const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
-      console.log("Found add-to-cart buttons:", addToCartButtons.length); 
+    console.log("Found add-to-cart buttons:", addToCartButtons.length);
     for (const button of addToCartButtons) {
         button.addEventListener('click', handleProduct);
+
     }
 }
 
 function handleProduct(e) {
+    if (nbItems >= 0) {
+        nbItemsElement.classList.remove("d-none");
+    }
     e.preventDefault();
     const button = e.currentTarget;
     const productCard = button.closest('.product-card');
@@ -20,5 +26,7 @@ function handleProduct(e) {
     const productImage = productCard.dataset.productImage;
     const productDescription = productCard.dataset.productDescription;
     const productCategory = productCard.dataset.productCategory;
+    nbItems++;
+    nbItemsElement.textContent = nbItems;
     console.log('Product added to cart:', productName, productPrice, productImage, productDescription, productCategory);
 }
